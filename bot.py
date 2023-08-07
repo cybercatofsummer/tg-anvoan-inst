1282918a51df83c7726dc2a4050c16a06bb06bb9
Andrey Smirnov <andrey.smirnov1@ringcentral.com>
2023-08-08 01:12:06 +0200

    db added

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, Updater, Callb
 import logging
 import json
 import os
-from queue import Queue
+import sqlite3
 
 
 MY_TG_CHAT_ID = os.getenv('MY_TG_CHAT_ID')
@@ -27,8 +27,6 @@ class MyChatIdFilter(MessageFilter):
 
 filterByMyChatId = MyChatIdFilter(MY_TG_CHAT_ID)
 
-image_queue = Queue()
-
 def start(update: Update, context):
     update.message.reply_text("Send me your image!")
 
@@ -41,8 +39,16 @@ def handle_image(update: Update, context):
     # Extracting the message_id
     message_id = sent_message.message_id
 
-    approve_data = json.dumps({"action": "approve", "chat_id": chat_id, "message_id": message_id})
-    reject_data = json.dumps({"action": "reject", "chat_id": chat_id, "message_id": message_id})
+    conn = sqlite3.connect('submissions.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO submissions (user_chat_id, admin_message_id, image_file_id) VALUES (?, ?, ?)',
+                   (chat_id, message_id, image.file_id))
+    conn.commit()
+    record_id = cursor.lastrowid
+    conn.close()
+
+    approve_data = json.dumps({"action": "approve", "record_id": record_id})
+    reject_data = json.dumps({"action": "reject", "record_id": record_id})
 
     keyboard = InlineKeyboardMarkup([
         [InlineKeyboardButton("Approve", callback_data=approve_data),
@@ -62,30 +68,36 @@ def handle_callback(update: Update, context):
 
     data = json.loads(query.data)
     action = data['action']
-    chat_id = data['chat_id']
-    message_id = data['message_id']
+
+    record_id = data['record_id']
+
+    submission = getRecordById(record_id)
 
     if action == 'approve':
-        return handleApprove(query, context, chat_id)
+        return handleApprove(query, context, submission['user_chat_id'], submission['admin_message_id'], record_id)
     if action == 'reject':
-        return handleReject(query, context, chat_id, message_id)
+        return handleReject(query, context, submission['user_chat_id'], submission['admin_message_id'], record_id)
 
 
-def handleApprove(query, context, chat_id):
+def handleApprove(query, context, chat_id, message_id, record_id):
     query.message.reply_text("Please enter the description for post:")
-    context.user_data['chat_id_to_notify'] = chat_id
-    return APPROVE
+    return handleRequest(APPROVE, context, chat_id, message_id, record_id)
 
-def handleReject(query, context, chat_id, message_id):
+def handleReject(query, context, chat_id, message_id, record_id):
     query.message.reply_text("Please enter the reason for rejection:")
-    context.user_data['chat_id_to_reject'] = chat_id
+    return handleRequest(REJECTION_REASON, context, chat_id, message_id, record_id)
+
+def handleRequest(action, context, chat_id, message_id, record_id): 
+    context.user_data['chat_id'] = chat_id
     context.user_data['message_id_to_delete'] = message_id
-    return REJECTION_REASON
+    context.user_data['record_id'] = record_id
+    return APPROVE if action == APPROVE else REJECTION_REASON
 
 def handle_rejection_reason(update: Update, context):
     rejection_reason = update.message.text
-    chat_id_to_reject = context.user_data['chat_id_to_reject']
+    chat_id_to_reject = context.user_data['chat_id']
     message_id_to_delete = context.user_data['message_id_to_delete']
+    record_id = context.user_data['record_id']
 
     # Delete the original message (you'll need the message_id)
     context.bot.delete_message(chat_id=MY_TG_CHAT_ID, message_id=message_id_to_delete)
@@ -96,20 +108,27 @@ def handle_rejection_reason(update: Update, context):
     # Notify the user that their submission was rejected
     context.bot.send_message(chat_id=chat_id_to_reject, text=f"Your submission was rejected for the following reason: {rejection_reason}")
 
-    return ConversationHandler.END
-
+    deleteRecordById(record_id)
+    
+    return PHOTO if hasPendingSubmissions() else ConversationHandler.END
 
 def handle_approve(update: Update, context):
     description = update.message.text
-    chat_id_to_notify = context.user_data['chat_id_to_notify']
+    chat_id_to_notify = context.user_data['chat_id']
+    message_id_to_delete = context.user_data['message_id_to_delete']
+    record_id = context.user_data['record_id']
 
     # Use the description to create the post (e.g., post to Instagram)
 
+    # Delete the original message (you'll need the message_id)
+    context.bot.delete_message(chat_id=MY_TG_CHAT_ID, message_id=message_id_to_delete)
 
     # Notify the user that their submission was rejected
     context.bot.send_message(chat_id=chat_id_to_notify, text=f"Your submission was approved and will be posted on Instagram with the following description: {description}")
 
-    return ConversationHandler.END
+    deleteRecordById(record_id)
+    
+    return PHOTO if hasPendingSubmissions() else ConversationHandler.END
 
 def handle_text(update: Update, context):
     update.message.reply_text("Please send me a photo, not text. Start over by sending a photo!")
@@ -117,6 +136,25 @@ def handle_text(update: Update, context):
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def getRecordById(record_id):
+    conn = sqlite3.connect('submissions.db')
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM submissions WHERE id = ?', (record_id,))
+    submission = cursor.fetchone()
+    conn.close()
+
+    return dict(submission) if submission else None
+
+
+def deleteRecordById(record_id):
+    conn = sqlite3.connect('submissions.db')
+    cursor = conn.cursor()
+
+    cursor.execute('DELETE FROM submissions WHERE id = ?', (record_id,))
+    conn.commit()
+    conn.close()
 
 def main():
     updater = Updater(BOT_TOKEN)
@@ -138,5 +176,28 @@ def main():
     updater.start_polling()
     updater.idle()
 
+def hasPendingSubmissions():
+    conn = sqlite3.connect('submissions.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT COUNT(*) FROM submissions')
+    count = cursor.fetchone()[0]
+    conn.close()
+    return count > 0
+
+def init_db():
+    conn = sqlite3.connect('submissions.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS submissions (
+        id INTEGER PRIMARY KEY,
+        user_chat_id INTEGER,
+        admin_message_id INTEGER,
+        image_file_id TEXT
+    )
+    ''')
+    conn.commit()
+    conn.close()
+
 if __name__ == '__main__':
+    init_db()
     main()
